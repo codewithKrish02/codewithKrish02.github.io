808de75d54f8e8dd1979bea3a078c69940f757e1
Teja Ravipudi <48492413+codewithKrish02@users.noreply.github.com>
2023-11-12 16:45:52 -0600

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -26,7 +26,7 @@ export default function Home() {
             <nav className='py-10 mb-12 flex justify-between'>
               <h1 className='text-3xl font-burtons' >codewithKrish</h1>
               <ul className='flex items-center'>
-                <li><a href='https://codewithkrish02.github.io/blogs/' className='bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 rounded-md ml-8'>Blog</a></li>
+                
               </ul>
             </nav>
 
