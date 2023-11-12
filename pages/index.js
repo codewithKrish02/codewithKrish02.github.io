7723206ef214ac02b555ffd9d150bd9445b3e87f
Teja Ravipudi <48492413+codewithKrish02@users.noreply.github.com>
2023-11-12 16:41:54 -0600

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -26,8 +26,8 @@ export default function Home() {
             <nav className='py-10 mb-12 flex justify-between'>
               <h1 className='text-3xl font-burtons' >codewithKrish</h1>
               <ul className='flex items-center'>
-                {/* <li><BsFillMoonStarsFill className='cursor-pointer text-2xl'/></li> */}
-  {/*<li><a href='https://codewithkrish02.github.io/Blogs/' className='bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 rounded-md ml-8'>Blog</a></li>*/}
+                <li><BsFillMoonStarsFill className='cursor-pointer text-2xl'/></li>
+  {             <li><a href='https://codewithkrish02.github.io/Blogs/' className='bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 rounded-md ml-8'>Blog</a></li>
               </ul>
             </nav>
 
